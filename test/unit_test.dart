c0650584b502a220b80bc94165b1918e672a6110
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-03-31 23:37:19 +0530

    learn: unit test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,16 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_flutter/main.dart';
 
 void main(){
-
-  test('Is Even',(){
-    bool result = isEven(12);
-    expect(result, true);
-    });
+  group('Iseven Group',(){
+    test('Is Even',(){
+      bool result = isEven(12);
+        expect(result, true);
+      });
     
     test( 'Is Odd',(){
       bool result = isEven(123);
-      expect(result, false);
-  }); 
-
+        expect(result, false);
+    }); 
+  });
 
 }
