423090388b0dd12158e91cf4d1ad4914da8ef437
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-04-04 00:01:24 +0530

    placing widget one after another

--- android/app/src/main/kotlin/com/example/learn_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/learn_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hello_flutter
+package com.example.learn_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import './basic_screen.dart';
-void main() => runApp(const StaticApp());
+import './profile_screen.dart';
 
-class StaticApp extends StatelessWidget{
-  const StaticApp({super.key});
-  
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context){
-      return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: BasicScreen(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
+    );
   }
 }
