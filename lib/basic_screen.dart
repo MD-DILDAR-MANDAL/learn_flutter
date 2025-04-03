7b5a2ce819ba742e8fdf1862d60c93a70b1f9e78
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-04-03 20:06:58 +0530

    feat: add stylish text on the screen

--- lib/basic_screen.dart
+++ lib/basic_screen.dart
@@ -1,37 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/immutable_widget.dart';
-
+import './immutable_widget.dart';
+import './text_layout.dart';
 class BasicScreen extends StatelessWidget {
   const BasicScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-        title: const Text('Welcome to Flutter'),
-        actions: const[
+      appBar: AppBar( 
+         backgroundColor: Colors.indigo,
+        title: Text('Welcome to Flutter'),
+        actions: <Widget>[
           Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Icon(Icons.edit),
           ),
         ],
-      ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 1.0,
-          child: ImmutableWidget(),
+       ),
+      body: Column(
+        children: const [ 
+          AspectRatio(
+            aspectRatio: 1.0,
+            child: ImmutableWidget(),
           ),
-      ),
+      TextLayout(),
+        ],
+    ),
       drawer: Drawer(
-        child: Container(
+         child: Container(
           color: Colors.lightBlue,
-          child: const Center(
-            child: Text("I am a Drawer  "),
+          child: Center(
+            child: Text("I'm a Drawer!"),
           ),
         ),
-      ),
+       ),
     );
   }
 }
