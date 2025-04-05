732f2660dee7e4bc47f129e6c0969b02a6a3094f
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-04-05 23:59:38 +0530

    draw shape with custom paint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/flex_screen.dart';
-
+import 'profile_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const FlexScreen(),
+      home: const ProfileScreen(),
       
     );
   }
