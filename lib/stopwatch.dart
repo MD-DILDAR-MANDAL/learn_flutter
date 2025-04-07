90279b6a3b34befa7210b6180c56038097b89109
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-04-08 00:02:01 +0530

    scroll updated

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -14,6 +14,8 @@ class _StopWatchState extends State<StopWatch>{
   int milliseconds = 0;
   late Timer timer;
   final laps = <int>[];
+  final itemHeight = 60.0;
+  final scrollController = ScrollController();
 
   @override
 
@@ -25,18 +27,27 @@ class _StopWatchState extends State<StopWatch>{
     } 
   }
   Widget _buildLapDisplay(){
-    return ListView(
-      children: [
-        for(int milliseconds in laps)
-          ListTile(
-            title: Text(_secondsText(milliseconds)),
-          ),
-      ],
+    return Scrollbar(
+      controller: scrollController,
+      child: ListView.builder(
+        controller: scrollController,
+        itemExtent: itemHeight,
+        itemCount: laps.length,
+        itemBuilder: (context, index){
+          final milliseconds = laps[index];
+          return ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 50),
+            title: Text('Lap ${index + 1}'),
+            trailing: Text(_secondsText(milliseconds)),
+          );
+        }        
+      ),
     );
   }
   @override
   void dispose(){
     timer.cancel();
+    scrollController.dispose();
     super.dispose();
   }
   
@@ -65,6 +76,11 @@ class _StopWatchState extends State<StopWatch>{
       laps.add(milliseconds);
       milliseconds = 0;
     });
+    scrollController.animateTo(
+      itemHeight*laps.length,
+      duration: Duration(milliseconds:500), 
+      curve: Curves.easeIn,
+      );
   }
 
   @override
